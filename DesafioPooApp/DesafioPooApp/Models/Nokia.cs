acfc3ae0d47cd2d96044ce8cf8973fa603289dc1
J-IgorSilva <aleatoria8821@gmail.com>
2022-10-07 08:38:20 -0300

    Desafio PooBotcamp

---
+++ DesafioPooApp/DesafioPooApp/Models/Nokia.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioPooApp.Models
+{
+    // TODO: Herdar da classe "Smartphone"
+    public class Nokia
+    {
+        // TODO: Sobrescrever o método "InstalarAplicativo"
+    }
+}
