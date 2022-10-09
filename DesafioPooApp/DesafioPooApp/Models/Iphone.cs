44cb8573f612f83eddde24d62239bcde6633ffc2
J-IgorSilva <aleatoria8821@gmail.com>
2022-10-09 08:12:03 -0300

    Override metodo
    sobrescrevendo metodo InstalarAplicativo

--- DesafioPooApp/DesafioPooApp/Models/Iphone.cs
+++ DesafioPooApp/DesafioPooApp/Models/Iphone.cs
@@ -7,8 +7,17 @@ using System.Threading.Tasks;
 namespace DesafioPooApp.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+
+        }
     }
 }
