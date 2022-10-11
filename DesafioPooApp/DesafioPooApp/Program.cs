c2cdc9ce488d1f604fd65025b29dfbd20ecc8122
J-IgorSilva <aleatoria8821@gmail.com>
2022-10-11 08:54:33 -0300

    instanciando objeto
    testando as funcionalidades.

--- DesafioPooApp/DesafioPooApp/Program.cs
+++ DesafioPooApp/DesafioPooApp/Program.cs
@@ -7,7 +7,15 @@ namespace DesafioPooApp.Models
         static void Main(string[] args)
         {
             Iphone celular = new Iphone("85985998584", "XR2022", "431233212343243", 256);
-            Console.WriteLine("Hello World!");
+            celular.Ligar();
+            Console.WriteLine(celular.Numero,celular.Modelo,celular.IMEI,celular.Memoria);
+            celular.ReceberLigacao();
+            
+            Console.WriteLine("Qual aplicativo deseja instalar");
+            string nome = Console.ReadLine();
+            
+            celular.InstalarAplicativo(nome);
+            
         }
     }
 }
